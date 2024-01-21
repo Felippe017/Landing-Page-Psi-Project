b4a598528a7a439b907f872d4ff9669ad8aa7178
Felippe017 <felippecorrea2010@gmail.com>
2024-01-21 14:47:30 -0300

    feat: Cria menu hamburguer na mobile responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,47 +1,100 @@
 import logo from '@/assets/logo-psi.jpeg';
+import { useState } from 'react';
 
 export const Header = (): React.JSX.Element => {
-  return (
-    <header className="sticky top-0 z-40 flex h-[80px] bg-[#526534] px-4 text-[#ffffff]">
-      <div className="flex flex-1 items-center justify-between">
-        <a
-          href="/"
-          rel="noreferrer"
-          className="relative z-10 hidden flex-1 justify-center text-xs md:flex"
-        >
-          <img src={logo} alt="logo" className="h-12 w-12" />
-        </a>
+  const [open, setOpen] = useState<boolean>(false);
 
-        <nav className="relative z-10 hidden flex-1 items-center justify-center text-neutral-700 md:flex dark:text-neutral-100">
-          <ul className="flex">
-            <li className="p-0 pr-14">
+  return (
+    <>
+      <header className="sticky top-0 z-40 flex h-[80px] bg-[#526534] px-4 text-[#ffffff]">
+        <div className="flex flex-1 items-center justify-between">
+          <a
+            href="/"
+            rel="noreferrer"
+            className="relative z-10 flex-1 justify-center text-xs md:flex"
+          >
+            <img src={logo} alt="logo" className="h-12 w-12" />
+          </a>
+
+          <div
+            onClick={() => {
+              setOpen(prevOpen => !prevOpen);
+            }}
+            className="space-y-2 md:hidden"
+          >
+            <div className="h-0.5 w-8 bg-[#B2BDA0]"></div>
+            <div className="h-0.5 w-8 bg-[#B2BDA0]"></div>
+            <div className="h-0.5 w-8 bg-[#B2BDA0]"></div>
+          </div>
+
+          <nav className="relative z-10 hidden flex-1 items-center justify-center px-4 text-neutral-700 md:flex dark:text-neutral-100">
+            <ul className="md:flex">
+              <li className="p-0 pr-14">
+                <a href="/" className="text-white hover:text-[#B2BDA0]">
+                  Home
+                </a>
+              </li>
+              <li className="p-0 pr-14">
+                <a
+                  href="/ansiedade"
+                  className="text-white hover:text-[#B2BDA0]"
+                >
+                  Ansiedade
+                </a>
+              </li>
+              <li className="p-0 pr-14">
+                <a
+                  href="/depressao"
+                  className="text-white hover:text-[#B2BDA0]"
+                >
+                  Depressão
+                </a>
+              </li>
+              <li className="p-0 pr-14">
+                <a href="/tdah" className="text-white hover:text-[#B2BDA0]">
+                  TDAH
+                </a>
+              </li>
+              <li className="p-0 pr-14">
+                <a href="/sobre" className="text-white hover:text-[#B2BDA0]">
+                  Sobre
+                </a>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      {!open && (
+        <div className={`md:hidden`}>
+          <ul className="w-[100%] border-t-4 border-solid border-[#2F3B1C] bg-[#526534] p-[5%]">
+            <li className="px-[5%] py-3">
               <a href="/" className="text-white hover:text-[#B2BDA0]">
                 Home
               </a>
             </li>
-            <li className="p-0 pr-14">
+            <li className="px-[5%] py-3">
               <a href="/ansiedade" className="text-white hover:text-[#B2BDA0]">
                 Ansiedade
               </a>
             </li>
-            <li className="p-0 pr-14">
+            <li className="px-[5%] py-3">
               <a href="/depressao" className="text-white hover:text-[#B2BDA0]">
                 Depressão
               </a>
             </li>
-            <li className="p-0 pr-14">
+            <li className="px-[5%] py-3">
               <a href="/tdah" className="text-white hover:text-[#B2BDA0]">
                 TDAH
               </a>
             </li>
-            <li className="p-0 pr-14">
+            <li className="px-[5%] py-3">
               <a href="/sobre" className="text-white hover:text-[#B2BDA0]">
                 Sobre
               </a>
             </li>
           </ul>
-        </nav>
-      </div>
-    </header>
+        </div>
+      )}
+    </>
   );
 };
